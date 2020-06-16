8bf07363b884ed7afecef1730e0a70799eedd9ec
root <root@localhost>
2020-06-16 19:07:31 -0400

    update zsh

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -18,6 +18,7 @@ zshrc="$(echo $HOME)/.zshrc"
 
 # exports
 echo "export TERM=xterm-256color" >> $zshrc
+echo "export UA='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'" >> $zshrc
 
 # aliases
 echo "alias lss='ls -ltr'" >> $zshrc
@@ -26,7 +27,6 @@ echo "alias P='git pull origin master'" >> $zshrc
 echo "alias p='git push origin master'" >> $zshrc
 echo "alias ipa='ip --brief --color a'" >> $zshrc
 echo "alias ips='sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 -u'" >> $zshrc
-echo "alias UA='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'" >> $zshrc
 echo "alias af='assetfinder -subs-only'" >> $zshrc
 echo "alias fn='fuzznav'" >> $zshrc
 
